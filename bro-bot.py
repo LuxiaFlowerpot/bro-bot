2301cbfa8080a8568d31887592973fbd3a51f088
LuxiaFlowerpot <40546556+LuxiaFlowerpot@users.noreply.github.com>
2018-08-17 20:11:37 +0200

    Create bro-bot.py

---
+++ bro-bot.py
@@ -0,0 +1,16 @@
+import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="bro-bot")
+
+
+@bot_event
+async def on_ready():
+    print('Connecté en tant que')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('--------------------')
+    await bot.change_presence(game=discord.Game(name="combattre Vega"))
+
+bot.run(os.getenv('Token'))
